527349935953d84fa8a1e831ae691e03245eb38d
Heorhii Honcharov <0019@ukr.net>
2024-08-19 18:16:27 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -14,8 +14,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           <BrowserRouter basename="/goit-react-hw-08-phonebook">
-            <App />
             <Global styles={GlobalStyle} />
+            <App />
           </BrowserRouter>
         </PersistGate>
       </Provider>
